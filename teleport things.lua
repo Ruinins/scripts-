2c4accbf0c388463bbd3f93f544beccec7ef6ff3
Ruinins <142075272+Ruinins@users.noreply.github.com>
2023-10-28 15:53:11 +0700

    Update and rename teleport things to teleport things.lua

--- teleport things.lua
+++ teleport things.lua
@@ -5,13 +5,13 @@ local w = library:CreateWindow("Teleport")
 local b = w:CreateFolder("Teleport") 
 
 b:Button("Teleport tool",function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Ruinins/scripts/main/Tp%20Tool'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Ruinins/scripts/main/Tp%20Tool.lua'))()
 end)
 
 b:Button("Perm teleport tool",function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Ruinins/scripts/main/Perm%20tp%20tool'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Ruinins/scripts/main/Perm%20tp%20tool.lua'))()
 end)
 
 b:Button("Ctrl + click teleport",function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Ruinins/scripts-/main/ctrl%20%2B%20click%20script'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Ruinins/scripts-/main/ctrl%20%2B%20click%20script.lua'))()
 end)
