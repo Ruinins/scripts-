140bbf65102ecc661d5389a331b82c8dc122d77b
Ruinins <142075272+Ruinins@users.noreply.github.com>
2023-11-03 23:17:33 +0700

    Create Hide UIs.lua

---
+++ Hide UIs.lua
@@ -0,0 +1,41 @@
+local player = game.Players.LocalPlayer
+local userInputService = game:GetService("UserInputService")
+
+local function hideScreenGui()
+    for _, gui in ipairs(player.PlayerGui:GetChildren()) do
+        if gui:IsA("ScreenGui") then
+            gui.Enabled = false
+        end
+    end
+end
+
+local function showScreenGui()
+    for _, gui in ipairs(player.PlayerGui:GetChildren()) do
+        if gui:IsA("ScreenGui") then
+            gui.Enabled = true
+        end
+    end
+end
+
+local function toggleScreenGui()
+    local screenGuiHidden = true
+
+    return function()
+        if screenGuiHidden then
+            showScreenGui()
+        else
+            hideScreenGui()
+        end
+        screenGuiHidden = not screenGuiHidden
+    end
+end
+
+local toggleFunction = toggleScreenGui()
+
+userInputService.InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.L then
+        toggleFunction()
+    end
+end)
+
+game.StarterGui:SetCore("SendNotification", {Title="UI hider"; Text="M to toggle"; Duration=6;})
